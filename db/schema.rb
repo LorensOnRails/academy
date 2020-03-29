d181a730bad74feb1b360b572d374a05344baa0d
LorensOnRails <lorensonrails@gmail.com>
2020-03-29 19:35:34 +0200

    Workshop model and controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_29_165443) do
+ActiveRecord::Schema.define(version: 2020_03_29_172630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,25 @@ ActiveRecord::Schema.define(version: 2020_03_29_165443) do
     t.index ["confirmation_token"], name: "index_accounts_on_confirmation_token", unique: true
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
+  end
+
+  create_table "workshops", force: :cascade do |t|
+    t.string "title"
+    t.string "subtitle"
+    t.integer "account_id"
+    t.string "slug"
+    t.text "image_data"
+    t.string "difficulty"
+    t.string "category"
+    t.string "language"
+    t.text "description"
+    t.text "requirements"
+    t.text "outcomes"
+    t.string "status"
+    t.decimal "cost"
+    t.integer "discount"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
