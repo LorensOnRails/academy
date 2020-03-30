26cbe0b9a365a3953ebc6e159ee4d8ea46bfc181
LorensOnRails <lorensonrails@gmail.com>
2020-03-31 01:06:12 +0200

    Curriculum in workshop

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,29 @@ Rails.application.routes.draw do
   devise_for :admins, :path => '', :path_names => {:sign_in => 'signin_as_admin', :sign_out => 'signout_as_admin'}, controllers: { sessions: 'admin/sessions' }
   devise_for :accounts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "public#index"
+  root to: "static_pages#dashboard"
+
+ resources :accounts
+ resources :workshops
+ resources :lessons do
+  collection do
+   patch :sort
+  end
+ end
+
+ resources :workshops do
+ resources :lessons
+end
+
+ namespace :admin do
+  root "static_pages#dashboard"
+  resources :static_pages
+  resources :accounts
   resources :workshops
-
-  namespace :admin do
-   resources :accounts
-   resources :workshops
+  resources :lessons
+  resources :workshops do
+   resources :lessons
   end
+ end
 
-  namespace :admin do
-	root "static_pages#dashboard"
-	resources :static_pages
-	resources :accounts
-	resources :workshops
-  end
 end
